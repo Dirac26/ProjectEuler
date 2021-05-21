d647902b3fdea7e04b714f22bf29db31268bd475
Dirac26 <muatasemoran@gmail.com>
2021-05-21 20:36:02 +0300

    added command line interface for problem 001

--- problem001/main.py
+++ problem001/main.py
@@ -1,3 +1,13 @@
+import sys
+import argparse
+
+def argparser(arg):
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('-input', required=True,type=int, 
+                    help='an integer for the accumulator')
+    args = parser.parse_args(arg)
+    return args
+
 def div_by35(num):
     div_list = []
     for n in range(num):
@@ -11,12 +21,13 @@ def sum_list(in_list):
         sum = sum + elm
     return sum
 
-def main():
-    print(sum_list(div_by35(1000)))
+def main(args):
+    print(sum_list(div_by35(args.input)))
 
 
 
 
 if __name__ == "__main__":
-    main()
+    ARGS = argparser(sys.argv[1:])
+    main(ARGS)
 
