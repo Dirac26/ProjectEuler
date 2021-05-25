d66d2af9f07d0275807ff85207b0f462f4d52f64
Dirac26 <muatasemoran@gmail.com>
2021-05-25 12:12:02 +0300

    added command interface for problem002

--- problem002/main.py
+++ problem002/main.py
@@ -1,3 +1,15 @@
+import sys
+import argparse
+
+def argparser(arg):
+    """ a method to parse arguments """
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('-input', required=True, type=int, 
+                    help='fib order')
+
+    args = parser.parse_args(arg)
+    return args
+
 def gen_fib_list(num):
     """function to generate fibonacci sequence capped by num for largest element"""
     a, b = 0, 1
@@ -18,8 +30,9 @@ def sum_list(in_list):
         sum = sum + elm
     return sum
 
-def main():
-    print(sum_list(even_fib_list(gen_fib_list(4000000))))
+def main(args):
+    print(sum_list(even_fib_list(gen_fib_list(args.input))))
 
 if __name__ == "__main__":
-    main()
+    ARGS = argparser(sys.argv[1:])
+    main(ARGS)
